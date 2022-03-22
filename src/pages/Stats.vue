c7ce22c01ed6449550ad63f676c9050aa841212d
bwreDev <colton@bwre.tech>
2022-03-21 20:42:17 -0700

    Draft of stats page.

--- src/pages/Stats.vue
+++ src/pages/Stats.vue
@@ -1,38 +1,65 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <div class="q-pa-md text-center" style="max-width: 600px">
-    <h3 class="">Last 30 days</h3>
+    <div class="flex items-start justify-start q-mt-md q-ml-md">
+      <q-select v-model="selectedYear" outlined :options="years" label="Year">
+      </q-select>
+    </div>
+  </div>
+
+  <!-- <div>
     <dl class="row justify-center q-gutter-md">
       <div v-for="item in stats" :key="item.name" class="stats">
         <dt class="text-sm font-medium text-gray-500 truncate">
           {{ item.name }}
         </dt>
         <dd class="mt-1 text-3xl font-semibold text-gray-900">
-          {{ item.stat }}
+          {{ selectedYear }}
         </dd>
       </div>
     </dl>
-  </div>
+  </div> -->
 </template>
 
-<script>
-const stats = [
-  { name: "Total Subscribers", stat: "71,897" },
-  { name: "Avg. Open Rate", stat: "58.16%" },
-  { name: "Avg. Click Rate", stat: "24.57%" },
-];
+<script setup>
+import { ref, computed } from "vue";
+import useMain from "../pinia/main";
 
-export default {
-  setup() {
-    return {
-      stats,
-    };
-  },
-};
+const store = useMain();
+
+const currentYear = new Date().getFullYear().toString();
+const years = computed(() => {
+  let years = [currentYear];
+  store.userBookLists.forEach((list) => {
+    if (!years.includes(list.year)) {
+      years.unshift(list.year);
+    }
+  });
+  return years;
+});
+const selectedYear = ref(currentYear);
+const selectedYearLists = computed(() => {
+  return store.userBookLists.filter((list) => {
+    return list.year === selectedYear.value;
+  });
+});
+
+const bookData = computed(() => {
+  if (store.statuses.length > 0) {
+    return books.value.map((book) => {
+      return {
+        id: book.id,
+        status: getReadingStatus(book.reading_status_id),
+        pages: book.pages,
+      };
+    });
+  } else {
+    return [];
+  }
+});
+
+// Here I am thinking that using the store is the best way to get the data we need.
+
+// I just need to work through how to combine the pages to get the total, and also the total amount of books.
 </script>
 
-<style>
-.stats {
-  background: darkgrey;
-}
-</style>
+<style></style>
