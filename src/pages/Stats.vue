01e3bd041bf36d70465f681267e5711a7690e86d
Brian Barrow <briancbarrow@gmail.com>
2022-04-11 16:51:47 -0600

    updated stats to show finished instead of currently reading

--- src/pages/Stats.vue
+++ src/pages/Stats.vue
@@ -69,7 +69,7 @@ async function getListBooks(listArray) {
     let { data } = await supabase
       .from("books")
       .select()
-      .eq("reading_status_id", 2)
+      .eq("reading_status_id", 3)
       .in("list_id", listArray);
     finished.value = data;
   } catch (err) {
